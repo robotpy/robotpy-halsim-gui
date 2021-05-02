69e57ca2b2f17f6528918146f2217db1c7f373bc
David Vo <auscompgeek@users.noreply.github.com>
2021-05-02 21:45:45 +1000

    Hack at wheel to override tag

--- setup.py
+++ setup.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
 
-from robotpy_build.setup import setup
 
-setup()
+from robotpy_build.setup import Setup
+
+s = Setup()
+s.prepare()
+
+try:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+except ImportError:
+    pass
+else:
+
+    class bdist_wheel(_bdist_wheel):
+        def get_tag(self):
+            self.root_is_pure = False
+            _impl, _abi_tag, plat_name = _bdist_wheel.get_tag(self)
+            self.root_is_pure = True
+            return ("py36", "none", plat_name)
+
+    s.setup_kwargs["cmdclass"]["bdist_wheel"] = bdist_wheel
+
+s.run()
